0923396ec43d4a820c14fd3417fc01fc5cae7226
azkali <azkali@localhost.localdomain>
2020-08-27 00:49:43 +0200

    Lint and enhance build script

--- l4t_kernel_prep_rel32.sh
+++ l4t_kernel_prep_rel32.sh
@@ -24,81 +24,79 @@ Prepare() {
 
 	# Handle Nvidia Kernel bits
 	echo "Extracting Nvidia Kernel Stuff"
-	mkdir -p ./kernel_r32/nvidia
-	mv ./l4t-kernel-nvidia*/* ./kernel_r32/nvidia
+	mkdir -p "${KERNEL_DIR}"/nvidia
+	mv ./l4t-kernel-nvidia*/* "${KERNEL_DIR}"/nvidia
 	rm -rf ./l4t-kernel-nvidia*
 	echo "Done"
 
 	#Handle Switchroot DTS files
 	echo "Extracting DTS stuff"
-	mkdir -p ./kernel_r32/hardware/nvidia/platform/t210/icosa
-	cd l4t-platform-t210-switch || exit
-	cd "${BUILD_DIR}" || exit
-	mv ./l4t-platform-t210-switch*/* ./kernel_r32/hardware/nvidia/platform/t210/icosa/
+	mkdir -p "${KERNEL_DIR}"/hardware/nvidia/platform/t210/icosa
+	mv ./l4t-platform-t210-switch*/* "${KERNEL_DIR}"/hardware/nvidia/platform/t210/icosa/
 	rm -rf ./l4t-platform-t210-switch*
 	echo "Done"
 
 	# Extract and place nvidia bits
 	echo "Extracting Nvidia GPU Kernel Bits"
-	mkdir -p ./kernel_r32/nvgpu
+	mkdir -p "${KERNEL_DIR}"/nvgpu
 	mkdir linux-nvgpu
 	tar -xf "./linux-nvgpu-r32.2.2.tar.gz" -C linux-nvgpu --strip 1
 	rm "./linux-nvgpu-r32.2.2.tar.gz"
-	mv ./linux-nvgpu/* ./kernel_r32/nvgpu
+	mv ./linux-nvgpu/* "${KERNEL_DIR}"/nvgpu
 	rm -r linux-nvgpu
 	echo "Done"
 
 	echo "Extracting Tegra SOC Data"
-	mkdir -p ./kernel_r32/hardware/nvidia/soc/tegra/
+	mkdir -p "${KERNEL_DIR}"/hardware/nvidia/soc/tegra/
 	mkdir soc-tegra
 	tar -xf "./soc-tegra-rel32.2.2.tar.gz" -C soc-tegra --strip 1
 	rm "./soc-tegra-rel32.2.2.tar.gz"
-	mv ./soc-tegra/* ./kernel_r32/hardware/nvidia/soc/tegra/
+	mv ./soc-tegra/* "${KERNEL_DIR}"/hardware/nvidia/soc/tegra/
 	rm -r soc-tegra
 	echo "Done"
 
 	echo "Extracting T210 SOC Data"
-	mkdir -p ./kernel_r32/hardware/nvidia/soc/t210/
+	mkdir -p "${KERNEL_DIR}"/hardware/nvidia/soc/t210/
 	mkdir soc-tegra-t210
 	tar -xf "soc-tegra-t210-rel32.2.2.tar.gz" -C soc-tegra-t210 --strip 1
 	rm "soc-tegra-t210-rel32.2.2.tar.gz"
-	mv ./soc-tegra-t210/* ./kernel_r32/hardware/nvidia/soc/t210/
+	mv ./soc-tegra-t210/* "${KERNEL_DIR}"/hardware/nvidia/soc/t210/
 	rm -r soc-tegra-t210
 	echo "Done"
 
 	echo "Extracting Tegra Common Platform Data"
-	mkdir -p ./kernel_r32/hardware/nvidia/platform/tegra/common/
+	mkdir -p "${KERNEL_DIR}"/hardware/nvidia/platform/tegra/common/
 	mkdir platform-tegra-common
 	tar -xf "platform-tegra-common-rel32.2.2.tar.gz" -C platform-tegra-common --strip 1
 	rm "platform-tegra-common-rel32.2.2.tar.gz"
-	mv ./platform-tegra-common/* ./kernel_r32/hardware/nvidia/platform/tegra/common/
+	mv ./platform-tegra-common/* "${KERNEL_DIR}"/hardware/nvidia/platform/tegra/common/
 	rm -r platform-tegra-common
 	echo "Done"
 
 	echo "Extracting T210 Common Platform Data"
-	mkdir -p ./kernel_r32/hardware/nvidia/platform/t210/common/
+	mkdir -p "${KERNEL_DIR}"/hardware/nvidia/platform/t210/common/
 	mkdir common-t210
 	tar -xf "platform-tegra-t210-common-rel32.2.2.tar.gz" -C common-t210 --strip 1
 	rm "platform-tegra-t210-common-rel32.2.2.tar.gz"
-	mv ./common-t210/* ./kernel_r32/hardware/nvidia/platform/t210/common/
+	mv ./common-t210/* "${KERNEL_DIR}"/hardware/nvidia/platform/t210/common/
 	rm -r common-t210
 	echo "Done"
 }
 
 Build() {
 	echo "Preparing Source and Creating Defconfig"
-	cd "${BUILD_DIR}/${KERNEL_DIR}/kernel-4.9" || exit
+	cd "${KERNEL_DIR}/kernel-4.9" || exit
 	cp arch/arm64/configs/tegra_linux_defconfig .config
 
 	export KBUILD_BUILD_USER=user
 	export KBUILD_BUILD_HOST=custombuild
 
-	#Prepare Linux sources
+	# Prepare Linux sources
 	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make olddefconfig
 	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make prepare
 	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make modules_prepare
 
-	#Actually build kernel
+	# Actually build kernel
 	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make -j"${CPUS}" tegra-dtstree="../hardware/nvidia"
 
 	mkdir -p "${BUILD_DIR}"/Final/
@@ -109,37 +107,21 @@ Build() {
 	echo "Done"
 }
 
-# Store last arg as output dir if not in Docker
-out=$(realpath "${@:$#}")
+# Retrieve last argument as output directory
+BUILD_DIR="$(realpath "${@:$#}")"
+KERNEL_DIR="${BUILD_DIR}/kernel_r32"
 
-if [[ -z ${CROSS_COMPILE} ]]; then
-	echo "CROSS_COMPILE not set! Exiting.."
-	exit 1
-elif [[ ! -d ${out} ]]; then
-	echo "Not a valid directory! Exiting.."
-	exit 1
-fi
+[[ -z "${CROSS_COMPILE}" ]] && \
+	echo "CROSS_COMPILE not set! Exiting.." && exit 1
 
-# Set build dir path
-BUILD_DIR=$(realpath "${out}")
+[[ ! -d "${BUILD_DIR}" ]] && \
+	echo "Not a valid directory! Exiting.." && exit 1
 
-# Set kernel dirname
-KERNEL_DIR="kernel_r32"
+[[ ! "${CPUS}" =~ ^[0-9]{,2}$ || "${CPUS}" > $(nproc)  ]] && \
+	echo "${CPUS} cores out of range or invalid, CPUS cores avalaible: $(nproc) ! Exiting..." && exit 1
 
-# Donwload and prepare bits
-if [[ ! -d "${BUILD_DIR}/${KERNEL_DIR}=" ]]; then
-	Prepare
-else
-	echo "${BUILD_DIR}"/${KERNEL_DIR}" exists! Skipping file setup/download..."
-fi
+# Download and prepare bits
+if [[ ! -e "${KERNEL_DIR}" ]]; then Prepare;
+	else echo "${KERNEL_DIR} exists! Skipping file setup/download..."; fi
 
-# Check if cpus have been set correctly and build
-if [[ ! ${CPUS} =~ ^[0-9]{,2}$ ]]; then
-	echo "${CPUS} cores out of range, or invalid! Exiting..."
-	exit 1
-elif [[ ${CPUS} > $(nproc) ]]; then
-	echo "${CPUS} exceeds the number of CPUS cores avalaible: $(nproc)! Exiting..."
-	exit 1
-else
-	Build
-fi
+Build
