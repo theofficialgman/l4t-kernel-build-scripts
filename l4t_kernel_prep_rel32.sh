e6b65139fa2fc7e027aed7f3c56f0870dfb75faa
Azkali <a.ffcc7@gmail.com>
2020-09-18 02:26:52 +0200

    Fix: fix bad arch comparison.

--- l4t_kernel_prep_rel32.sh
+++ l4t_kernel_prep_rel32.sh
@@ -145,10 +145,10 @@ firmware_dir="${KERNEL_DIR}/firmware"
 # Retrieve KERNEL_BRANCH variables corresponding to the build file branch to checkout
 set -a && . "./KERNEL_VERSIONS" && set +a
 
-[[ -z ${ARCH} && $(uname -m) != ${ARCH} ]] && \
+[[ -z ${ARCH} ]] && \
 	echo "Target build ARCH not set! Exiting.." && exit 1
 
-[[ -z "${CROSS_COMPILE}" && $(uname -m) != ${ARCH} ]] && \
+[[ -z "${CROSS_COMPILE}" && $(uname -m) != "aarch64" ]] && \
 	echo "CROSS_COMPILE not set! Exiting.." && exit 1
 
 [[ ! -d "${BUILD_DIR}" ]] && \
