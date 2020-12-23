aa9491f1e4152ca137a9523b9cdfb7d29f891043
The Great Wizard Azkali <a.ffcc7@gmail.com>
2020-12-23 20:27:41 +0000

    l4t_kernel_prep_rel32.sh: Remove duplicate (damn smartphones...)

--- l4t_kernel_prep_rel32.sh
+++ l4t_kernel_prep_rel32.sh
@@ -142,20 +142,6 @@ Build() {
 	cd "${KERNEL_DIR}/kernel-4.9"
 	git checkout ${KERNEL_VER}
 
-ir -p "${BUILD_DIR}"
-	
-	cd "${KERNEL_DIR}/nvidia"
-	git checkout ${NVIDIA_VER}
-
-	cd "${KERNEL_DIR}/kernel-4.9"
-	git checkout ${KERNEL_VER}
-
- "${KERNEL_DIR}/nvidia"
-	git checkout ${NVIDIA_VER}
-
-	cd "${KERNEL_DIR}/kernel-4.9"
-	git checkout ${KERNEL_VER}
-
 	cp arch/arm64/configs/tegra_linux_defconfig .config
 
 	# Prepare Linux sources
