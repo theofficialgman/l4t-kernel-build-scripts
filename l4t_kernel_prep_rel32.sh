7761aa8c2fb3802798ce97fa5e61bb47540e803e
The Great Wizard Azkali <a.ffcc7@gmail.com>
2021-02-16 19:54:54 +0000

    Place current cross compiler first in PATH

--- l4t_kernel_prep_rel32.sh
+++ l4t_kernel_prep_rel32.sh
@@ -25,11 +25,14 @@ create_update_modules() {
 
 Prepare() {
 	mkdir -p "${KERNEL_DIR}/update" "${KERNEL_DIR}/modules"
+
+	# Download latest repo tool from google and initialize repo manifest
 	curl https://storage.googleapis.com/git-repo-downloads/repo-1 > repo
 	chmod a+x repo
 	python3 repo init -u . -m default.xml -b master
 	python3 repo sync --force-sync --jobs=${CPUS}
 
+	# Setup linaro aarch64 GCC7 for cross compilation
 	if [[ ! -d "${KERNEL_DIR}/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu" ]]; then
 		echo -e "\nSetting up aarch64 cross compiler"
 		wget -q -nc --show-progress https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
@@ -37,9 +40,11 @@ Prepare() {
 		rm gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
 	fi
 
-	PATH="$PATH:$(realpath ${KERNEL_DIR}/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu)/bin/"
+	# Set cross compiler in PATH
+	PATH="$(realpath ${KERNEL_DIR}/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu)/bin/:$PATH"
 }
 
+# Apply patch from patch dir if required
 Patch() {
 	if [[ ${PATCH} == "true" ]]; then
 		cd "${KERNEL_DIR}"
@@ -54,6 +59,8 @@ Build() {
 	echo "Preparing Source and Creating Defconfig"
 
 	cd "${KERNEL_DIR}/kernel-4.9"
+
+	# Copy kernel defconfig
 	cp arch/arm64/configs/tegra_linux_defconfig .config
 
 	# Prepare Linux sources
@@ -68,11 +75,15 @@ Build() {
 }
 
 PostConfig() {
+	# Refresh permissions for kernel headers
 	find "${KERNEL_DIR}/update/usr/include" -name *.install* -exec rm {} \;
 	find "${KERNEL_DIR}/update/usr/include" -exec chmod 777 {} \;
+
+	# Create compressed modules and headers
 	create_update_modules "${KERNEL_DIR}/modules/lib/" "${KERNEL_DIR}/modules.tar.gz"
 	create_update_modules "${KERNEL_DIR}/update/" "${KERNEL_DIR}/update.tar.gz"
 
+	# Copy kernel Image and device tree
 	cp arch/arm64/boot/Image arch/arm64/boot/dts/tegra210-icosa.dtb "${KERNEL_DIR}"
 
 	rm -rf "${KERNEL_DIR}/modules" "${KERNEL_DIR}/update/usr/include" \
